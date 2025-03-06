881b8526f8246f0ec0d671c7508af54f4ea41fee
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-03-06 14:00:22 +0530

    Add invoice deletion and update functionality with revalidation and redirection

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 import { z} from "zod"
 import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -13,6 +15,35 @@ const FormSchema = z.object({
 const sql = postgres(process.env.POSTGRES_URL!, { ssl:"require" });
 const CreateInvoice = FormSchema.omit({id : true, date: true});
 
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
 export async function createInvoice(formData: FormData) {
 
   const {customerId, amount, status} = CreateInvoice.parse({
@@ -24,5 +55,6 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split("T")[0]
   // ...server-side logic for creating an invoice...
   await sql `INSERT INTO invoices (customer_id, amount, status, date) VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`
- console.log("Invoice Created Successfully...")
+  revalidatePath("/dashboard/invoices/")
+  redirect('/dashboard/invoices/')
 }
